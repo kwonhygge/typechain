b06c0cf76725ecb940413b92e375bbadec767245
kwonhygge <kitteniso1103@gmail.com>
2020-03-18 22:34:51 +0900

    Check if block is valid and add it

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@
 import * as CryptoJS from "crypto-js";
 
 class Block{
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
 
     //static method -> block안에 있고 클래스를 만들지 않아도 호출할 수 있는 것
     static calculateBlockHash = (
@@ -17,6 +12,18 @@ class Block{
         data: string
         ): string => CryptoJS.SHA256(index+previousHash+timestamp+data).toString();
 
+    static validateStructure = (aBlock: Block) : boolean => 
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
     constructor(
         index: number,
@@ -64,10 +71,44 @@ const createNewBlock = (data:string) : Block =>{
         data,
         newTimestamp
         );
+        addBlock(newBlock);
         return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string => 
+Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data);
+
+const isBlockValid = (
+    candidateBlock : Block,
+    previousBlock: Block): boolean =>{
+        if(!Block.validateStructure(candidateBlock)){
+            return false;
+        }else if(previousBlock.index + 1 !== candidateBlock.index){
+            return false;
+        }else if(previousBlock.hash !== candidateBlock.previousHash){
+            return false;
+        }else if(getHashforBlock(candidateBlock) !== candidateBlock.hash){
+            return false;
+        }else{
+            return true;
+        }
+};
+
+const addBlock = (candidateBlock: Block) : void =>{
+    if(isBlockValid(candidateBlock, getLatestBlock())){
+        blockchain.push(candidateBlock);
+    }
 }
 
-console.log(createNewBlock("hello"),createNewBlock("bye bye"));
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 
 
